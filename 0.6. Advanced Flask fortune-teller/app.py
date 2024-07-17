3c39f1f7bb929619aa7a898bc1b934801042cbca
rotemre25-meet <chinkycreenel@gmail.com>
2024-07-17 17:20:13 +0300

    Fancy Fortune Teller
    With birth month and long inp

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
-	return render_template("home.html")
+	if request.method == 'GET':
+		return render_template("home.html")
+	if request.method == 'POST':
+		return redirect(url_for('/fortune', month = bm)) 
 
-@app.route('/fortune')
+
+@app.route('/fortune', methods = ('GET','POST'))
 def fortune():
-	fortunes = ["You will have a great day!","A surprise is waiting for you.","Today is your lucky day!","Happiness will find you.","Be cautious today.","You will meet someone special.","An opportunity will arise.","Expect the unexpected.","Good news is coming your way.","You will achieve your goals."]
-	num1 = random.randint(0,10)
-	chosen_fortune=fortunes[num1]
+	fortunes = ["You will have a great day!","A surprise is waiting for you.","Today is your lucky day!","Happiness will find you.","Be cautious today.","You will meet someone special.","An opportunity will arise.","Expect the unexpected.","Good news is coming your way.","You will achieve your goals.","You will be punished."]
+	if len(request.form['bm']) <= 9:
+		chosen_fortune=fortunes[len(request.form['bm'])]
+	else:
+		chosen_fortune = fortunes[10]
 	return render_template('fortune.html',fortune = chosen_fortune)
 
 if __name__ == '__main__':
